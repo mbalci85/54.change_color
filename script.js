b9b2579046bfe130bbf2adeae220323927bbbd9f
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-01-29 21:20:32 +0000

    added funcs and DOM elements

---
+++ script.js
@@ -0,0 +1,50 @@
+// In this Project, your web page background color will be changed randomly when you clicked button on the page.
+
+const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple'];
+
+let button1 = document.getElementById('button1');
+
+button1.onclick = () => {
+	let colorIndex = Math.floor(Math.random() * colors.length);
+	document.body.style.backgroundColor = colors[colorIndex];
+	document.getElementById('button1').innerHTML = colors[colorIndex];
+	document.getElementById('button1').style.backgroundColor =
+		colors[colorIndex];
+
+	document.querySelector('h1').innerHTML = colors[colorIndex];
+};
+
+let button2 = document.getElementById('button2');
+
+button2.onclick = () => {
+	let hexCodeArray = [
+		0,
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		'a',
+		'b',
+		'c',
+		'd',
+		'e',
+		'f',
+	];
+	let hexCode = ['#'];
+
+	for (let i = 0; i < 6; i++) {
+		hexCode.push(
+			hexCodeArray[Math.floor(Math.random() * hexCodeArray.length)],
+		);
+	}
+	console.log(hexCode.join(''));
+	document.body.style.backgroundColor = hexCode.join('');
+	document.getElementById('button2').innerHTML = hexCode.join('');
+	document.getElementById('button2').style.backgroundColor = hexCode.join('');
+	document.querySelector('h1').innerHTML = hexCode.join('');
+};
